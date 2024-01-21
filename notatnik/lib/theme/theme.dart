c95a7eb38a27dd91949651c1afdd852825243a8d
Kuba <kubakudlaty520@gmail.com>
2024-01-21 17:18:46 +0100

    Projekt

--- notatnik/lib/theme/theme.dart
+++ notatnik/lib/theme/theme.dart
@@ -3,19 +3,19 @@ import 'package:flutter/material.dart';
 ThemeData lightMode = ThemeData(
   brightness: Brightness.light,
   colorScheme: ColorScheme.light(
-    background: Colors.grey.shade300,
-    primary: Colors.grey.shade200,
-    secondary: Colors.grey.shade400,
-    inversePrimary: Colors.grey.shade800,
+    background: Colors.purple.shade300,
+    primary: Colors.purple.shade200,
+    secondary: Colors.purple.shade400,
+    inversePrimary: Colors.purple.shade800,
   ),
 );
 
 ThemeData darkMode = ThemeData(
   brightness: Brightness.dark,
   colorScheme: ColorScheme.dark(
-    background: Colors.grey.shade900,
-    primary: Colors.grey.shade800,
-    secondary: Colors.grey.shade700,
-    inversePrimary: Colors.grey.shade300,
+    background: Colors.purple.shade900,
+    primary: Colors.purple.shade800,
+    secondary: Colors.purple.shade700,
+    inversePrimary: Colors.purple.shade300,
   ),
 );
